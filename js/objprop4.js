982a14e6363a09d530fb56f376b7c77e163f2d5e
Seoha Lee <seohalee0131@gmail.com>
2024-01-30 01:21:43 +0900

    object & property practice

---
+++ js/objprop4.js
@@ -0,0 +1,14 @@
+const kim = {nid: 3, nm: 'Hong', addr: 'Pusan'};
+
+const newKim = copyObject(kim);
+newKim.addr = 'Daegu';
+console.log(kim.addr !== newKim.addr);
+console.log(newKim);
+
+function copyObject(obj){
+    const newObj = {};
+    for (const k in obj)
+        newObj[k] = obj[k];
+    return newObj;
+}
+
---
+++ js/objprop5.js
@@ -0,0 +1,46 @@
+// function makeObjectFromArray(arr){
+//     const rets=[];
+//     for(let k=0;k<arr.length;k+=1){
+//         rets.push([arr[k][0],[...arr[k]]]);
+//     }
+//     return rets;
+// }
+
+// function makeObjectFromArray(arr){
+//     const rets=[];
+//     for(let k=0;k<arr.length;k+=1){
+//         const [first, ...rest]=arr[k];
+//         rets.push([first, rest]);
+//     }
+//     return Object.fromEntries(rets);
+// }
+
+// console.log(Object.values(arr));
+
+const arr = [['A', 10, 20], ['B', 30, 40], ['C', 50, 60, 70]];
+const arr2 = { 'A': [10, 20], 'B': [30, 40], 'C': [50, 60, 70] };
+
+console.log('Object -> Array');
+function makeObjectFromArray(arr){
+    const rets=[];
+    for(k of arr){
+        const [first, ...rest]=k;
+        rets.push([first, rest]);
+    }
+    return Object.fromEntries(rets);
+}
+
+console.log(makeObjectFromArray(arr));
+
+console.log('-----------------------------------------');
+console.log('Array -> Object');
+
+function makeArrayFromObject(arr){
+    const rets=[];
+    for(k in arr2){
+        rets.push([k, ...arr2[k]])
+    }
+    return rets;
+}
+
+console.log(makeArrayFromObject(arr2));
